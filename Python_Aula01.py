f2afab248a1d4ff48bded1f24f27339f47a561ff
comedordeterra <jpedro26037@gmail.com>
2025-03-17 22:48:38 -0300

    Minha primeira aula em Python

--- Python_Aula01.py
+++ Python_Aula01.py
@@ -1,2 +1,29 @@
-
 print("Hello World!")
+# ===================================================================================
+# Exercício 1:
+print ("João Pedro de Oliveira Soares")
+# ===================================================================================
+# Exercício 2:
+a = 3
+b = 5
+print ("O valor é:",3*b*2*a)
+# ===================================================================================
+# Exercício 3:
+aa = 2
+bb = 14
+cc = 812
+
+print (aa+bb+cc)
+# CTRL + ? = Transforma a área selecionada em comentário
+# CTRL + D = Copia a última linha embaixo
+# ===================================================================================
+# Teste com inputs:
+num1 = int(input("Número 1 = "))
+num2 = int(input("Número 2 = "))
+num3 = int(input("Número 3 = "))
+print (num1+num2+num3)
+# float = números decimais e int = números inteiros
+# ===================================================================================
+# Formatando casas decimais:
+numero = 2.5674
+print (f"O número com duas casas decimais é: {numero:.2f}")
